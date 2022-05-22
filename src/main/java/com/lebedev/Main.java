a1ce9f0e1963acc51d39bf1250a7f09df946c72b
DerLed <48630545+DerLed@users.noreply.github.com>
2022-05-22 21:02:56 +0300

    first commit

--- src/main/java/com/lebedev/Main.java
+++ src/main/java/com/lebedev/Main.java
@@ -1,7 +1,33 @@
 package com.lebedev;
 
+import com.lebedev.dao.UserDao;
+import com.lebedev.entity.UserEntity;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
+        UserDao ud = new UserDao();
+        UserEntity user3 = new UserEntity("Василий", "888@mail.ru");
+        ud.saveUser(user3);
+        UserEntity user2 = ud.findUserByName("Антон").get();
+        ud.deleteUser(user2);
+        List<UserEntity> users = ud.findAllUser();
+
+        for (UserEntity u: users){
+            System.out.println(u);
+        }
+
+
+        UserEntity user1 = ud.findUserById(4L).get();
+
+        System.out.println(user1);
+
+
+
+        System.out.println(user2);
+
 
     }
 }
